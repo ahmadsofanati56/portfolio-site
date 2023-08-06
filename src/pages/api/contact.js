df71419c886044f88fd96257eec9b1af728ba943
ahmadsofanati56 <ahmadsofanati56@gmail.com>
2023-08-06 22:45:29 +0300

    final version

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-export default function (req, res) {
+export default  async (req, res) => {
   require('dotenv').config()
 
   let nodemailer= require('nodemailer')
@@ -15,6 +15,18 @@ export default function (req, res) {
     },
     secure:true
   })
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+        if (error) {
+            console.log(error);
+            reject(error);
+        } else {
+            console.log("Server is ready to take our messages");
+            resolve(success);
+        }
+    });
+});
 
   const mailData={
     from: 'asofanati145@gmail.com',
@@ -24,12 +36,17 @@ export default function (req, res) {
     html: `<div>${req.body.message}</div><p>Sent from:
     ${req.body.email}</p>`
   }
-  transporter.sendMail(mailData, function (err, info) {
-    if(err)
-      console.log(err)
-    else
-      console.log(info)
-   
-  })
+  await new Promise((resolve, reject) => {
+    // send mail
+    transporter.sendMail(mailData, (err, info) => {
+        if (err) {
+            console.error(err);
+            reject(err);
+        } else {
+            console.log(info);
+            resolve(info);
+        }
+    });
+});
   res.send('success')
 }
